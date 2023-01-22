9e111070cb5c8f919e67d8b8b4293a32da324bcf
PatrickJank <format97@wp.pl>
2023-01-22 19:06:26 +0100

    trying to create a class for actual projects. this will assign generated projects to it. Side note: i have no idea what i'm doing after ~9 hours of non-stop code

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 import BaseGame.DailyOption;
 import OtherClasses.NumberGenerator;
 import OtherClasses.TitleScreen;
+import Projects.Project;
 import Projects.ProjectTemplate;
 import settings.SettingsSelection;
 
@@ -14,7 +15,7 @@ SettingsSelection color = new SettingsSelection();
 DailyOption DailyOption1 = new DailyOption();
 ProjectTemplate projects = new ProjectTemplate();
 NumberGenerator test = new NumberGenerator();
-
+Project test1 = new Project("A", "A", true, true, true, true, true, true, "A", "A", 1, 1);
 //stateOption.WelcomeScreen();
 int readInput1 = 0;
 int readInput2 = 0;
@@ -22,7 +23,7 @@ int readInput3 = 0;
 
 
 
-
+test1.test();
         //actual game
 /*
         while(readInput1 != 1) {
--- src/Projects/Project.java
+++ src/Projects/Project.java
@@ -1,24 +1,54 @@
 package Projects;
 
 public class Project {
-        private String ProjectName ;
-        private String ProjectDescription;
-        private boolean FrontEnd;
-        private boolean Backend;
-        private boolean mobile;
-        private boolean dataBase;
-        private boolean wordpress;
-        private boolean prestashop;
-        private char client;
-        private char Deadline;
-        private int Penalty;
-        private int PaycheckDate;
 
-    public Project() {
+    public String ProjectName ;
+        public String ProjectDescription;
+        public boolean FrontEnd;
+        public boolean Backend;
+        public boolean mobile;
+        public boolean dataBase;
+        public boolean wordpress;
+        public boolean prestashop;
+        public String client;
+        public String Deadline;
+        public int Penalty;
+        public int PaycheckDate;
 
+    public Project(String ProjectName, String ProjectDescription, boolean FrontEnd, boolean Backend, boolean mobile, boolean dataBase, boolean wordpress, boolean prestashop, String client, String Deadline, int Penalty, int PaycheckDate) {
+this.ProjectName = ProjectName;
+this.ProjectDescription = ProjectDescription;
+this.FrontEnd = FrontEnd;
+this.Backend = Backend;
+this.mobile = mobile;
+this.dataBase = dataBase;
+this.wordpress = wordpress;
+this.prestashop = prestashop;
+this.Penalty = Penalty;
+this.PaycheckDate = PaycheckDate;
     }
 
-
-
+public String test(){/*
+    if(FrontEnd == true){
+        System.out.println("FrontEnd knowledge is a must.");
+    }
+    if(Backend == true){
+        System.out.println("Backend knowledge is a must.");
+    }
+    if(mobile == true){
+        System.out.println("mobile knowledge is a must.");
+    }
+    if(dataBase == true){
+        System.out.println("dataBase knowledge is a must.");
+    }
+    if(wordpress == true){
+        System.out.println("wordpress knowledge is a must.");
+    }
+    if(prestashop == true){
+        System.out.println("Prestashop knowledge is a must.");
+    }
+    */
+       return(ProjectName+ " " + ProjectDescription + " " + Deadline + " " + Penalty + " " + PaycheckDate);
+}
 
 }
