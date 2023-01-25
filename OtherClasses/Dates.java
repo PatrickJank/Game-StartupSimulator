43b160d360eadb680025077d6224b1f824e0ddc3
PatrickJank <format97@wp.pl>
2023-01-25 21:21:22 +0100

    I was joking, they should be working now?

--- OtherClasses/Dates.java
+++ OtherClasses/Dates.java
@@ -15,20 +15,21 @@ public class Dates {
     private static final DateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
     private static final DateTimeFormatter dateFormat8 = DateTimeFormatter.ofPattern(DATE_FORMAT);
 
+    // Get current date
+    Date currentDate = new Date();
+
+
     public void pleaseWork() {
 
-        // Get current date
-        Date currentDate = new Date();
-        System.out.println("date : " + dateFormat.format(currentDate));
 
         // convert date to localdatetime
         LocalDateTime localDateTime = currentDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
         System.out.println("localDateTime : " + dateFormat8.format(localDateTime));
 
         // plus one
-        localDateTime = localDateTime.plusYears(1).plusMonths(1).plusDays(1);
-        localDateTime = localDateTime.plusHours(1).plusMinutes(2).minusMinutes(1).plusSeconds(1);
-
+        localDateTime = localDateTime.plusYears(-3).plusMonths(0);
+        //by using local time and various technics i can set a starting time, assign it to a variable and then add days off of it. Ez pz
+       System.out.println(localDateTime.getDayOfMonth());
         // convert LocalDateTime to date
         Date currentDatePlusOneDay = Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
 
